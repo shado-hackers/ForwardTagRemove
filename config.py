d9d288abc344e3808182e62766309c7eac138d6f
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2022-05-22 08:44:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,6 +20,5 @@ class Config:
 	
  TOKEN=os.environ.get("BOT_TOKEN",None)
  SOURCE="https://t.me/shado_hackers"
- START_TEXT=""Hey {}. \n\nWelcome to {} \n\nI  am A Forward Tag remover Bot.Send /help \n\nBy\n © @OMG_info"
+ START_TEXT="Hi [{}](tg://user?id={})\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do \n @shado_hackers"
  HELP_TEXT="Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n\n`How to Set Caption?`\nReply Caption to a File,Photo,Audio,Media"
-	
